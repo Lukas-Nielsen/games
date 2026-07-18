9ce8dc9aa9227102e6866c3a8869a9e061776cd8
Lukas Nielsen <moin@lukasnielsen.de>
2026-07-18 15:05:36 +0200

    vp check: ignore build dir

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
 	lint: {
 		options: { typeAware: true, typeCheck: true },
 		plugins: ["eslint", "import", "oxc", "node", "react", "typescript"],
-		ignorePatterns: ["postcss.config.cts"],
+		ignorePatterns: ["postcss.config.cts", "build"],
 		rules: {
 			"react/exhaustive-deps": "off",
 			"eslint/no-unused-vars": ["warn", { fix: { imports: "fix", variables: "suggestion" } }],
@@ -30,6 +30,7 @@ export default defineConfig({
 				"unknown",
 			],
 		},
+		ignorePatterns: ["build"],
 		sortPackageJson: true,
 		printWidth: 150,
 	},
